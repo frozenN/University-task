cd693ff53b794d8f3a45b824f1a0f03fb49a35cc
Gabriel Erzinger Dousseau <erzingergabriel@outlook.com>
2017-03-28 11:13:14 -0300

    Add files via upload

--- clienteudp.c
+++ clienteudp.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
@@ -15,94 +16,101 @@
 #define REMOTE_SERVER_PORT 1500
 #define MAX_MSG 100
 
+int sd, rc, i;
+struct sockaddr_in cliAddr, remoteServAddr;
+struct hostent *h;
+char nomearq[10], myfile[10101], texto;
+int strpos = 0;
 
-int main(int argc, char *argv[]) {
+void ssend(int argc, char *argv[]){
 
-  int sd, rc, i;
-  struct sockaddr_in cliAddr, remoteServAddr;
-  struct hostent *h;
-
-  /* check command line args */
-  if(argc<3) {
-    printf("usage : %s <server> <data1> ... <dataN> \n", argv[0]);
-    exit(1);
-  }
-
-  /* get server IP address (no check if input is IP address or DNS name */
-  h = gethostbyname(argv[1]);
-  if(h==NULL) {
-    printf("%s: unknown host '%s' \n", argv[0], argv[1]);
-    exit(1);
-  }
-
-  printf("%s: sending data to '%s' (IP : %s) \n", argv[0], h->h_name,
-	 inet_ntoa(*(struct in_addr *)h->h_addr_list[0]));
-
-  remoteServAddr.sin_family = h->h_addrtype;
-  memcpy((char *) &remoteServAddr.sin_addr.s_addr,
-	 h->h_addr_list[0], h->h_length);
-  remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);
-
-  /* socket creation */
-  sd = socket(AF_INET,SOCK_DGRAM,0);
-  if(sd<0) {
-    printf("%s: cannot open socket \n",argv[0]);
-    exit(1);
-  }
-
-  /* bind any port */
-  cliAddr.sin_family = AF_INET;
-  cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  cliAddr.sin_port = htons(0);
-
-  rc = bind(sd, (struct sockaddr *) &cliAddr, sizeof(cliAddr));
-  if(rc<0) {
-    printf("%s: cannot bind port\n", argv[0]);
-    exit(1);
-  }
-
-
-  /* send data */
-  for(i=2;i<argc;i++) {
-    rc = sendto(sd, argv[i], strlen(argv[i])+1, 0,
-		(struct sockaddr *) &remoteServAddr,
-		sizeof(remoteServAddr));
-
-    if(rc<0) {
-      printf("%s: cannot send data %d \n",argv[0],i-1);
-      close(sd);
+    if(argc<3) {
+      printf("usage : %s <server> <data1> ... <dataN> \n", argv[0]);
       exit(1);
     }
 
+    h = gethostbyname(argv[1]);
+    if(h==NULL) {
+      printf("%s: unknown host '%s' \n", argv[0], argv[1]);
+      exit(1);
+    }
+
+    printf("%s: sending data to '%s' (IP : %s) \n", argv[0], h->h_name,
+     inet_ntoa(*(struct in_addr *)h->h_addr_list[0]));
+
+
+    remoteServAddr.sin_family = h->h_addrtype;
+    memcpy((char *) &remoteServAddr.sin_addr.s_addr,
+  	 h->h_addr_list[0], h->h_length);
+    remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);
+
+    sd = socket(AF_INET,SOCK_DGRAM,0);
+    if(sd<0) {
+      printf("%s: cannot open socket \n",argv[0]);
+      exit(1);
+    }
+
+    cliAddr.sin_family = AF_INET;
+    cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    cliAddr.sin_port = htons(0);
+
+    rc = bind(sd, (struct sockaddr *) &cliAddr, sizeof(cliAddr));
+    if(rc<0) {
+      printf("%s: cannot bind port\n", argv[0]);
+      exit(1);
+    }
+    rc = sendto(sd, myfile, strlen(myfile)+1, 0,
+    	    (struct sockaddr *) &remoteServAddr,
+    		    sizeof(remoteServAddr));
+
+    if(rc<0) {
+          printf("%s: cannot send data %d \n",argv[0],i-1);
+          close(sd);
+          exit(1);
+        }
+
+}
+
+void rread(char *argv[])
+{
+  memcpy(nomearq, argv[2], sizeof(nomearq));
+  FILE* arq;
+  arq = fopen(nomearq,"rt");
+  if(arq == NULL)
+  {
+    printf("Nao e possivel abrir o arquivo\n");
+    exit(1);
   }
+  while((fscanf(arq,"%c", &texto)) != EOF)
+  {
+    myfile[strpos] = texto;
+    strpos++;
+  }
+  fclose(arq);
+}
 
+void receiveConfirm(char *argv[])
+{
   while(1) {
-
-    /* init buffer */
     char msg[MAX_MSG];
     memset(msg,0x0,MAX_MSG);
-
-
-    /* receive message */
     int cliLen = sizeof(cliAddr);
     int n = recvfrom(sd, msg, MAX_MSG, 0,
 		 (struct sockaddr *) &cliAddr, &cliLen);
-
     if(n<0) {
       printf("%s: cannot receive data \n",argv[0]);
       continue;
     }
-
-    /* print received message */
     printf("%s: from %s:UDP%u : %s \n",
 	   argv[0],inet_ntoa(cliAddr.sin_addr),
 	   ntohs(cliAddr.sin_port),msg);
      break;
-     /* add return msg*/
+   }
+}
 
-  }/* end of server infinite loop */
-
-
+int main(int argc, char *argv[]) {
+  rread(argv);
+  ssend(argc, argv);
+  receiveConfirm(argv);
   return 1;
-
 }
--- servidorudp.c
+++ servidorudp.c
@@ -10,24 +10,27 @@
 #include <stdio.h>
 #include <unistd.h> /* close() */
 #include <string.h> /* memset() */
+#include <stdlib.h>
 
 #define LOCAL_SERVER_PORT 1500
 #define MAX_MSG 100
 
-int main(int argc, char *argv[]) {
+int sd, rc, n, cliLen;
+struct sockaddr_in cliAddr, servAddr;
+char msg[MAX_MSG];
+FILE *arq;
+char nome_arquivo[20];
+int i;
+char msgzinha[100];
 
-  int sd, rc, n, cliLen;
-  struct sockaddr_in cliAddr, servAddr;
-  char msg[MAX_MSG];
-
-  /* socket creation */
+void receive(int argc, char *argv[])
+{
   sd=socket(AF_INET, SOCK_DGRAM, 0);
   if(sd<0) {
     printf("%s: cannot open socket \n",argv[0]);
     exit(1);
   }
 
-  /* bind local server port */
   servAddr.sin_family = AF_INET;
   servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servAddr.sin_port = htons(LOCAL_SERVER_PORT);
@@ -41,47 +44,56 @@ int main(int argc, char *argv[]) {
   printf("%s: waiting for data on port UDP %u\n",
 	   argv[0],LOCAL_SERVER_PORT);
 
-  /* server infinite loop */
   while(1) {
 
-    /* init buffer */
     memset(msg,0x0,MAX_MSG);
 
 
-    /* receive message */
     cliLen = sizeof(cliAddr);
     n = recvfrom(sd, msg, MAX_MSG, 0,
 		 (struct sockaddr *) &cliAddr, &cliLen);
+
+    makefile();
 
     if(n<0) {
       printf("%s: cannot receive data \n",argv[0]);
       continue;
     }
 
-    /* print received message */
-    printf("%s: from %s:UDP%u : %s \n",
-	   argv[0],inet_ntoa(cliAddr.sin_addr),
-	   ntohs(cliAddr.sin_port),msg);
-     /* add return msg*/
 
-     int i;
-     char msgzinha[100];
-     memcpy(msgzinha, "Ola cliente, sua mensagem foi recebida", 100);
-     /* send data */
-       rc = sendto(sd, msgzinha, strlen(msgzinha)+1, 0,
-      (struct sockaddr *) &cliAddr,
-      sizeof(cliAddr));
+          memcpy(msgzinha, "Ola cliente, sua mensagem foi recebida", 100);
 
-       if(rc<0) {
-         printf("%s: cannot send data %d \n",argv[0],i-1);
-         close(sd);
-         exit(1);
-       }
-       break;
+          rc = sendto(sd, msgzinha, strlen(msgzinha)+1, 0,
+            (struct sockaddr *) &cliAddr,sizeof(cliAddr));
+
+          if(rc<0)
+          {
+               printf("%s: cannot send data %d \n",argv[0],i-1);
+               close(sd);
+               exit(1);
+          }
+             break;
 
 
-  }/* end of server infinite loop */
+   }
 
-return 0;
+}
+
+void makefile()
+{
+  strcpy(nome_arquivo, "saida.txt");
+  arq = fopen(nome_arquivo, "wt");
+  if(arq == NULL)
+  {
+     printf("Nao e possivel iniciar o arquivo\n");
+     exit(1);
+  }
+  fprintf(arq,msg);
+  fclose(arq);
+}
+
+int main(int argc, char *argv[]) {
+    receive(argc, argv);
+    return 0;
 
 }
